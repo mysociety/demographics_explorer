04f118584670dcdddbb9dfb468ebb57e0e6c7f1b
Alex Parsons <alex@alexparsons.co.uk>
2021-03-24 16:12:44 +0000

    Removed custom bake manager

--- explorer/bake.py
+++ explorer/bake.py
@@ -3,25 +3,4 @@ from dirsync import sync
 
 from django.conf import settings
 
-from django_sourdough.views import BaseBakeManager
-
-
-class BakeManager(BaseBakeManager):
-    """
-    add support for copying media files
-    """
-
-    def copy_media_files(self):
-        dir_loc = os.path.join(settings.BAKE_LOCATION, "sites", "media")
-        print("syncing media")
-        if os.path.isdir(dir_loc) == False:
-            os.makedirs(dir_loc)
-        sync(settings.MEDIA_ROOT, dir_loc, "sync", ctime=True)
-
-    def amend_settings(self, **kwargs):
-        super(BakeManager, self).amend_settings(**kwargs)
-        settings.IS_LIVE = True
-
-    def bake_app(self):
-        self.app_urls.bake() #only_absent=True
-        #self.copy_media_files()
+pass
